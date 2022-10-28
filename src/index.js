e42c2c6a88de63dda0beb510647642db195340f7
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2022-10-27 20:44:59 -0600

    feat: text area web component, pretty JSON

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+const before = document.getElementById("before");
+const pretty = document.getElementById("pretty");
+const after = document.getElementById("after");
+const error = document.getElementById("error");
+
+pretty.addEventListener(
+  "click",
+  function () {
+    try {
+      const b = before.children[1].value;
+      const parsed = JSON.parse(b);
+      after.children[1].value = JSON.stringify(parsed, null, 2);
+    } catch (e) {
+      error.innerText = e.message;
+    }
+  },
+  false
+);
