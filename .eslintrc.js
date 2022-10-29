f3713c1f865ee0c2bea1edd37ca9df2f6eace3be
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2022-10-29 15:23:07 -0600

    chore: use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   env: { browser: true, es2021: true, node: true },
   extends: ["eslint:recommended", "prettier", "plugin:sonarjs/recommended"],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+  },
   plugins: ["simple-import-sort", "sonarjs"],
   rules: {
     "no-await-in-loop": "warn",
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import babel from "prettier/parser-babel";
+import graphQl from "prettier/parser-graphql";
+import html from "prettier/parser-html";
+import markdown from "prettier/parser-markdown";
+import postcss from "prettier/parser-postcss";
+import typescript from "prettier/parser-typescript";
+import yaml from "prettier/parser-yaml";
+import { format } from "prettier/standalone";
+
+const formatSelect = document.getElementById("format");
 const before = document.getElementById("before");
 const pretty = document.getElementById("pretty");
 const after = document.getElementById("after");
@@ -6,8 +16,13 @@ const error = document.getElementById("error");
 pretty.addEventListener(
   "click",
   function () {
+    error.innerText = "";
+
     try {
-      after.value = JSON.stringify(JSON.parse(before.value), null, 2);
+      after.value = format(before.value, {
+        parser: formatSelect.value,
+        plugins: [babel, graphQl, html, markdown, postcss, typescript, yaml],
+      });
     } catch (e) {
       error.innerText = e.message;
     }
